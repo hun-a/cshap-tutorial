ed5de81447dee9ae6796ca1a842d30babf2d580b
hun-a <seunghun.chan@gmail.com>
2020-03-18 22:22:59 +0900

    Add the initialization grammar to the Property syntax

--- src/property/property.cs
+++ src/property/property.cs
@@ -5,14 +5,11 @@ namespace Property {
   class Runner {
 
     class Creature {
-      public int Hp {
-        get; set;
-      }
+      public int Hp { get; set; } = 100;
     }
 
     public static void Main(string[] args) {
       Creature creature = new Creature();
-      creature.Hp = 100;
       Console.WriteLine(creature.Hp);
     }
   }
