73ecba33935c87d3f609617cf115c2d566bcfaed
hun-a <seunghun.chan@gmail.com>
2020-03-18 22:18:00 +0900

    Add the Property tutorial

--- src/property/property.cs
+++ src/property/property.cs
@@ -4,8 +4,16 @@ namespace Property {
 
   class Runner {
 
+    class Creature {
+      public int Hp {
+        get; set;
+      }
+    }
+
     public static void Main(string[] args) {
-      
+      Creature creature = new Creature();
+      creature.Hp = 100;
+      Console.WriteLine(creature.Hp);
     }
   }
 }
